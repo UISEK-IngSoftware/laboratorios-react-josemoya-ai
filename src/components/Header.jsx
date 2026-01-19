b7135e296db85b4bdea99f967df7f88dbe35be51
josemoya-ai <jose.moya@uisek.edu.ec>
2026-01-19 15:30:01 -0500

    Finalizacion del lavoratorio 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,40 @@
 import { AppBar, Container, Toolbar, Button } from "@mui/material";
 import pokedexLogo from "../assets/pokedex-logo.png";
-
+import { logout } from "../services/authService";
 import './Header.css';
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+    const isLoggedIn = localStorage.getItem('access_token') !== null;
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logout();
+        alert("Cierre de sesión exitoso");
+        navigate('/');
+    };
+
     return (
         <header className="pokedex-navbar">
-            <Container>
-                <AppBar position="static">
-                    <Toolbar>
-                        <div className="image-container">
-                            <img src={pokedexLogo} alt="Pokédex Logo" height={100} />
-                        </div>
-                    </Toolbar>
-                    <Toolbar>
-                        <Button color="inherit" href="/">Inicio</Button>
-                        <Button color="inherit" href="/add-pokemon">Agregar Pokémon</Button>
-                    </Toolbar>
-                </AppBar>
-            </Container>
+
+            <AppBar position="static">
+                <Toolbar>
+                    <div className="image-container">
+                        <img src={pokedexLogo} alt="Pokédex Logo" height={100} />
+                    </div>
+                </Toolbar>
+                <Toolbar>
+                    <Button color="inherit" href="/">Inicio</Button>
+                    {isLoggedIn && 
+                        <>
+                            <Button color="inherit" href="/add-pokemon">Agregar Pokémon</Button>
+                            <Button color="inherit" onClick={handleLogout}>Cerrar Sesión</Button>
+                        </>
+                    }
+                    {!isLoggedIn && <Button color="inherit" href="/login">Iniciar Sesión</Button>}
+                </Toolbar>
+            </AppBar>
+
         </header>
     )
 }
--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -2,8 +2,58 @@ import axios from 'axios';
 
 const Api_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+/**
+ * Obtener la lista de pokemones
+ * @return
+ */
 export async function fetchPokemons() {
     const response = await axios.get(`${Api_BASE_URL}/pokemons`);
     console.log(response);
+    return response.data;
+}
+
+/**
+ * Convertir un archivo a Base64
+ * @param {} file 
+ * @returns 
+ */
+function fileToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      // reader.result ya incluye el encabezado, lo usamos completo
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+/**
+ * Crear un nuevo Pokémon
+ * @param {*} pokemonData 
+ * @returns 
+ */
+export async function addPokemon(pokemonData) {
+    let pictureBase64 = '';
+    if (pokemonData.picture) {
+        pictureBase64 = await fileToBase64(pokemonData.picture);
+    }
+    const payload = {
+        ...pokemonData,
+        picture: pictureBase64
+    };
+    const response = await axios.post(
+        `${Api_BASE_URL}/pokemons/`, 
+        payload
+    );
     return response.data;
 }
